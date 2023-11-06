08b0e83196f0264db19e4c5c757baaa59a7cb528
fawwasaldy <fawwas.aldy2111@gmail.com>
2023-11-06 08:46:10 +0700

    fixing js

--- pokemon.js
+++ pokemon.js
@@ -17,14 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
                     for (let j = i; j < i+4; j++) {
                         const pokemon = pokemonList[j];
                         const colDiv = document.createElement("div");
-                        colDiv.className = "col-xl-3 col-md-3 mb-0";
-
+                        colDiv.className = "col-xl-3 col-md-3 col-sm-3 mb-0";
+                        const pokemonHurufBesar = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
                         colDiv.innerHTML = `
-                            <div class="card border-primary my-2">
+                            <div class="card border-primary shadow my-2">
                                 <div class="card-body">
                                     <div class="row no-gutters">
                                         <div class="col mr-0">
-                                            <div class="h5 mb-0 mr-0 font-weight-bold text-gray-800">${pokemon.name}</div>
+                                            <div class="h5 mb-2 mr-0 font-weight-bold text-gray-800">${pokemonHurufBesar}</div>
                                             <a href="index.html" class="btn btn-primary btn-user">
                                                 Detail
                                             </a>
