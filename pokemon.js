347821316deb355f0a098e6277e50d221be54ce4
fawwasaldy <fawwas.aldy2111@gmail.com>
2023-11-06 08:33:17 +0700

    update js

--- pokemon.js
+++ pokemon.js
@@ -7,22 +7,37 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(data => {
             if (data.hasOwnProperty('results')) {
                 const pokemonList = data.results;
-                const listItem = document.createElement("div");
-                pokemonList.forEach(pokemon => {
-                    listItem.className = "col-xl-3 col-md-3 mb-4";
-                    listItem.innerHTML += `
-                    <div class="card o-hidden border-0 shadow-lg my-5">
-                        <div class="card-body">
-                            <div class="row no-gutters container text-center">
-                                <div class="col mr-2">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">${pokemon.name}</div>
+
+                const container = document.createElement("div");
+                container.className = "container text-center";
+
+                for (let i = 0; i < pokemonList.length; i += 4) {
+                    const row = document.createElement("div");
+                    row.className = "row";
+                    for (let j = i; j < i+4; j++) {
+                        const pokemon = pokemonList[j];
+                        const colDiv = document.createElement("div");
+                        colDiv.className = "col-xl-3 col-md-3 mb-0";
+
+                        colDiv.innerHTML = `
+                            <div class="card o-hidden border-0 shadow-lg my-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters">
+                                        <div class="col mr-0">
+                                            <div class="h5 mb-0 mr-0 font-weight-bold text-gray-800">${pokemon.name}</div>
+                                            <a href="index.html" class="btn btn-primary btn-user">
+                                                Detail
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                    `;
-                    apiDataList.appendChild(listItem);
-                });
+                        `;
+                        row.appendChild(colDiv);
+                    }
+                    container.appendChild(row);
+                }
+                apiDataList.appendChild(container);
             }
             
         })
